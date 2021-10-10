77e06b04a425b8e7adf4f80b186ab3c5d7ef8e6c
Lenni <lennivh24@gmail.com>
2021-10-10 11:35:24 +0200

    fixed term lauchner for paths with spaces

--- termLauncher.sh
+++ termLauncher.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-termite -d $((echo "~"; for proc in $(ps -C zsh -o pid --no-headers); do
-	realpath /proc/$proc/cwd
-done) | sed -e "s?$HOME?~?g" | xargs -n 1 | sort -u | rofi -dmenu | sed -e "s?~?$HOME?g")
+# Using ? as list separator, as they can't appear in paths
+
+folders=$(
+	printf "~?\n";
+	for proc in $(ps -C zsh -o pid --no-headers); do
+		realpath /proc/$proc/cwd | sed -e "s?$HOME?~?g"
+		printf "?"
+	done
+)
+
+echo "before sorting:" > /tmp/folders
+echo $folders >> /tmp/folders
+
+termite -d $(echo "$folders" | tr '?' '\n' | sort -u | rofi -dmenu | sed -e "s?~?$HOME?g")
